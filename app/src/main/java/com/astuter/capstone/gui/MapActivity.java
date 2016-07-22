55a56d6677d0c015803c5becf0fd373422d59187
Aniruddh <anu.bhilvare@gmail.com>
2016-07-22 18:37:40 +0530

    fix: Solve issue of Place detail image animation

--- app/src/main/java/com/astuter/capstone/gui/MapActivity.java
+++ app/src/main/java/com/astuter/capstone/gui/MapActivity.java
@@ -9,7 +9,6 @@ import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.CursorLoader;
@@ -17,7 +16,8 @@ import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.*;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
